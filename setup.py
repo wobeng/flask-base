37b26041cadaed855c460d6a79bdf736d519cb7c
Welby Obeng <wobeng@yblew.com>
2017-08-06 16:39:24 -0400

    refactored

--- setup.py
+++ setup.py
@@ -1,58 +1,14 @@
-import ast
-import sys
-from subprocess import call
-
-import os
-import re
-from setuptools import setup, find_packages
-from setuptools.command.install import install
-
-
-def package_meta():
-    """Read __init__.py for global package metadata.
-    Do this without importing the package.
-    """
-    _version_re = re.compile(r'__version__\s+=\s+(.*)')
-    _url_re = re.compile(r'__url__\s+=\s+(.*)')
-    _license_re = re.compile(r'__license__\s+=\s+(.*)')
-
-    with open('flask_base/__init__.py', 'rb') as ffinit:
-        initcontent = ffinit.read()
-        version = str(ast.literal_eval(_version_re.search(
-            initcontent.decode('utf-8')).group(1)))
-        url = str(ast.literal_eval(_url_re.search(
-            initcontent.decode('utf-8')).group(1)))
-        licencia = str(ast.literal_eval(_license_re.search(
-            initcontent.decode('utf-8')).group(1)))
-    return {
-        'version': version,
-        'license': licencia,
-        'url': url,
-    }
-
-
-_lu_meta = package_meta()
-
-
-class MyInstall(install):
-    def run(self):
-        install.run(self)
-        requirements_file = os.path.join(os.getcwd(), "requirements.txt")
-        if os.path.isfile(requirements_file):
-            cmd = [sys.executable.replace("python", "pip"), "install", "-r", requirements_file]
-            call(cmd)
-
+from distutils.core import setup
 
 setup(
     name='flask-base',
-    description='flask base',
-    url=_lu_meta['url'],
-    author='Welby Obeng',
-    license=_lu_meta['license'],
-    keywords='flask base',
-    packages=find_packages(),
-    version=_lu_meta['version'],
-    cmdclass={'install': MyInstall},
+    version='1.0.0',
+    packages=['flask_base'],
+    url='https://github.com/wobeng/flask-base',
+    license='',
+    author='wobeng',
+    author_email='wobeng@yblew.com',
+    description='flask base app',
     install_requires=[
         'Flask',
         'marshmallow',
@@ -60,6 +16,5 @@ setup(
         'flasgger',
         'apispec',
         'PyYAML'
-    ],
-    dependency_links=['git+https://git@github.com/wobeng/aws-utils.git@master']
+    ]
 )
