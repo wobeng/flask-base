e16fe0ad9a0dc50f1fddbe889460b3e6e8f2db19
yblew <wobeng@yblew.com>
2017-08-16 14:08:03 -0400

    refactored

--- flask_base/reqdata.py
+++ flask_base/reqdata.py
@@ -1,16 +1,12 @@
 from flask import request
 
 
-def multi_dict_marsh(data):
-    return {key: value.split(',') for key, value in data.items()}
-
-
 def request_body():
-    return request.get_json(True, True) or multi_dict_marsh(request.form)
+    return request.get_json(True, True) or request.form
 
 
 def request_query():
-    return multi_dict_marsh(request.args)
+    return request.args
 
 
 def request_header():
@@ -18,7 +14,6 @@ def request_header():
         key.lower().replace('-', '_'): val
         for key, val in request.headers
     }
-
 
 def request_view_arg():
     return dict(request.view_args)
