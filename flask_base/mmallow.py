ca3297d1ee0c84d81a9aa489a75b0d4b0e3cd086
yblew <wobeng@yblew.com>
2020-06-21 13:33:46 -0400

    added user schema

--- flask_base/mmallow.py
+++ flask_base/mmallow.py
@@ -1,20 +1,19 @@
-import os
-import re
-import traceback
-from datetime import datetime
-from urllib.parse import parse_qs
-
 import bcrypt
 import dateutil.parser
+import os
 import phonenumbers
+import re
 import requests
 import simplejson
+import traceback
 import validators
+from datetime import datetime
 from dateutil.rrule import rrulestr
 from jsonschema import Draft7Validator
 from marshmallow import fields, validate
 from more_itertools import unique_everseen
 from pytz import UTC
+from urllib.parse import parse_qs
 from validate_email import validate_email as validate_email_func
 
 from flask_base.swagger import mm_plugin
