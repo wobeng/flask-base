9a1276b529900695912d5303ac9907f1b5701e5f
welby <welby@caseactive.com>
2024-11-04 11:49:16 +0000

    refactor

--- flask_base/app.py
+++ flask_base/app.py
@@ -35,7 +35,7 @@ def init_api(
     app = Flask(name, instance_relative_config=True, subdomain_matching=True)
 
     # init reserve proxy
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_port=1)
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.wsgi_app = CloudfrontProxy(app.wsgi_app)
 
     # init cors
